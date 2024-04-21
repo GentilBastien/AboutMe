8ff369c781b731c85b3c75c9723ad479865b00d3
GentilBastien <bastien.ga@free.fr>
2024-04-21 17:27:29 +0200

    init page not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -16,5 +17,9 @@ export const routes: Routes = [
         loadChildren: () => import('./pages/project/project.module').then(m => m.ProjectModule),
       },
     ],
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
   },
 ];
